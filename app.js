398dabd021dcdabff95db444cbdc027276b64847
NirvanaBradford <nirvanabradford06@gmail.com>
2025-07-30 08:52:21 -0400

    feat: added for loop

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const flavours = [
+// froyoShop array for flavours
+const order = [
   "vanilla",
   "vanilla",
   "vanilla",
@@ -7,7 +8,24 @@ const flavours = [
   "coffee",
 ];
 
-console.log(flavours);
+// creating an object to fill with order count
+const flavourCount = {};
+
+for (let i = 0; i < order.length; i++) {
+  const flavours = order[i];
+  // does the key exist in my object
+  console.log(flavourCount);
+
+  if (flavours in flavourCount) {
+    // add one to the count
+    flavourCount[flavours] += 1;
+    // otherwise if it doesn't
+  } else {
+    flavourCount[flavours] = 1;
+  }
+}
+
+console.log(flavourCount);
 
 const userInputString = prompt(
   "Please enter some integers separated by commas.",
