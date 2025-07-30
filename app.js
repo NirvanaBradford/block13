e44fb4138cddaf9985df0ce4ed0d1a58b378af32
NirvanaBradford <nirvanabradford06@gmail.com>
2025-07-30 08:19:29 -0400

    feat: created js file

---
+++ app.js
@@ -0,0 +1,13 @@
+const flavours = [
+  "vanilla",
+  "vanilla",
+  "vanilla",
+  "strawberry",
+  "coffee",
+  "coffee",
+];
+
+const userInputString = prompt(
+  "Please enter some integers separated by commas.",
+  "vanilla, vanilla, vamilla, strawberry, coffee, coffee"
+);
